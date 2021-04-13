97397129bc9e86384a1806d010f3b6371482b294
Akarsh Saraff <saraffakarsh@gmail.com>
2021-04-13 18:43:52 +0100

    redeploy the structured

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 // use the express-static middleware
-app.use(express.static("../AkarshSaraff-portfolio-site"));
+app.use(express.static("public"));
 
 // define the first route
 app.get("/", function (req, res) {
